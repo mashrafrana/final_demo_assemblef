8936ebaed21f18403eb3dea7b55201abbc580173
gilanigilani <abdullah@pixarsart.com>
2021-02-04 15:45:42 +0500

    go live testing

--- src/components/EditVideoGrid/index.tsx
+++ src/components/EditVideoGrid/index.tsx
@@ -70,60 +70,85 @@ export const EditVideoGrid: React.FC<Props> = ({ isSetting }) => {
         method: 'live_broadcast',
         phase: 'create',
       },
-      createRes => {
-        let mediaRecorder;
-        let mediaStream;
+      function(response) {
+        if (!response.id) {
+          alert('dialog canceled');
+          return;
+        }
+        alert(`{your-stream-url}:${response.secure_stream_url}`);
         FB.ui(
           {
             display: 'popup',
             method: 'live_broadcast',
             phase: 'publish',
-            broadcast_data: createRes,
+            broadcast_data: response,
           },
-          publishRes => {
-            console.log(publishRes);
+          function(response) {
+            alert(`video status: \n${response.status}`);
           }
         );
-        const ws = new WebSocket(
-          `${window.location.origin.replace(
-            'http',
-            'ws'
-          )}/rtmp/${encodeURIComponent(createRes.stream_url)}`
-        );
-        ws.addEventListener('open', e => {
-          console.log('WebSocket Open', e);
-          mediaStream = document.querySelector('canvas').captureStream(60); // 30 FPS
-          const AudioContext = window.AudioContext || window.webkitAudioContext;
-          const audioCtx = new AudioContext();
-          const dest = audioCtx.createMediaStreamDestination();
-          const localAudioStream: any =
-            audioVideo.realtimeController.state.audioInput;
-          const localAudio = audioCtx.createMediaStreamSource(localAudioStream);
-          localAudio.connect(dest);
-          const audio: any = audioVideo.audioMixController.audioStream;
-          if (audio) {
-            const partAudio = audioCtx.createMediaStreamSource(audio);
-            partAudio.connect(dest);
-          }
-          if (dest.stream.getAudioTracks().length > 0) {
-            mediaStream.addTrack(dest.stream.getAudioTracks()[0]);
-          }
-          mediaRecorder = new MediaRecorder(mediaStream, {
-            mimeType: 'video/webm;codecs=h264',
-            videoBitsPerSecond: 3000000,
-          });
-          mediaRecorder.addEventListener('dataavailable', e => {
-            ws.send(e.data);
-          });
-          mediaRecorder.addEventListener('stop', ws.close.bind(ws));
-          mediaRecorder.start(1000); // Start recording, and dump data every second
-        });
-        ws.addEventListener('close', e => {
-          console.log('WebSocket Close', e);
-          mediaRecorder.stop();
-        });
       }
     );
+    // FB.ui(
+    //   {
+    //     display: 'popup',
+    //     method: 'live_broadcast',
+    //     phase: 'create',
+    //   },
+    //   createRes => {
+    //     let mediaRecorder;
+    //     let mediaStream;
+    //     FB.ui(
+    //       {
+    //         display: 'popup',
+    //         method: 'live_broadcast',
+    //         phase: 'publish',
+    //         broadcast_data: createRes,
+    //       },
+    //       publishRes => {
+    //         console.log(publishRes);
+    //       }
+    //     );
+    //     const ws = new WebSocket(
+    //       `${window.location.origin.replace(
+    //         'http',
+    //         'ws'
+    //       )}/rtmp/${encodeURIComponent(createRes.stream_url)}`
+    //     );
+    //     ws.addEventListener('open', e => {
+    //       console.log('WebSocket Open', e);
+    //       mediaStream = document.querySelector('canvas').captureStream(60); // 30 FPS
+    //       const AudioContext = window.AudioContext || window.webkitAudioContext;
+    //       const audioCtx = new AudioContext();
+    //       const dest = audioCtx.createMediaStreamDestination();
+    //       const localAudioStream: any =
+    //         audioVideo.realtimeController.state.audioInput;
+    //       const localAudio = audioCtx.createMediaStreamSource(localAudioStream);
+    //       localAudio.connect(dest);
+    //       const audio: any = audioVideo.audioMixController.audioStream;
+    //       if (audio) {
+    //         const partAudio = audioCtx.createMediaStreamSource(audio);
+    //         partAudio.connect(dest);
+    //       }
+    //       if (dest.stream.getAudioTracks().length > 0) {
+    //         mediaStream.addTrack(dest.stream.getAudioTracks()[0]);
+    //       }
+    //       mediaRecorder = new MediaRecorder(mediaStream, {
+    //         mimeType: 'video/webm;codecs=h264',
+    //         videoBitsPerSecond: 3000000,
+    //       });
+    //       mediaRecorder.addEventListener('dataavailable', e => {
+    //         ws.send(e.data);
+    //       });
+    //       mediaRecorder.addEventListener('stop', ws.close.bind(ws));
+    //       mediaRecorder.start(1000); // Start recording, and dump data every second
+    //     });
+    //     ws.addEventListener('close', e => {
+    //       console.log('WebSocket Close', e);
+    //       mediaRecorder.stop();
+    //     });
+    //   }
+    // );
   };
   const toggleLogo = () => {
     const logo = document.getElementById('logo').value;
