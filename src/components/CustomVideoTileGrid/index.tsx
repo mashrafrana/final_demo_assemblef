fd331ea5e39cee8a5b1e53c7f0225b956dd64351
gilanigilani <abdullah@pixarsart.com>
2021-02-25 04:12:28 +0500

    fb live testing

--- src/components/CustomVideoTileGrid/index.tsx
+++ src/components/CustomVideoTileGrid/index.tsx
@@ -377,12 +377,12 @@ export const CustomVideoTileGrid: React.FC<Props> = ({
               else if(video.length === 3) {
                 for (let i = 0; i < video.length; i ++){
                     if(i === 0)
-                      ctx.drawImage(video[i], 110, 0,490, 500,  0, 0, 426, 720);
+                      ctx.drawImage(video[i], 150, 0,420, 460,  0, 0, 426, 720);
                   else if (i === 1) {
-                    ctx.drawImage(video[i], 110, 0,490, 500,  426, 0, 426, 720); 
+                    ctx.drawImage(video[i], 150, 0,420, 460,  426, 0, 426, 720); 
                   }
                   else if (i === 2) {
-                    ctx.drawImage(video[i], 110, 0,490, 500,  852, 0, 426, 720); 
+                    ctx.drawImage(video[i], 110, 0,420, 460,  852, 0, 426, 720); 
                   }
                 }
               }
